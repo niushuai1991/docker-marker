9b4cccf45fb9b838852b9cc3497b296b04522f10
niushuai1991 <niushuai1991@foxmail.com>
2025-03-04 14:48:47 +0800

    改为英文

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 
-# PYPI url
+# Check if the POETRY_PYPI_URL environment variable is set
 if [ -n "$POETRY_PYPI_URL" ]; then
-    echo "配置 Poetry 使用镜像源: $POETRY_PYPI_URL"
+    echo "Configuring Poetry to use mirror source: $POETRY_PYPI_URL"
     poetry config repositories.pypi "$POETRY_PYPI_URL"
 else
-    echo "未设置 POETRY_PYPI_URL，使用默认 PyPI 源"
+    echo "POETRY_PYPI_URL is not set, using the default PyPI source"
 fi
 
+echo "Installing dependencies..."
 poetry install --project=/opt/marker
 if [ $? -ne 0 ]; then
     echo "Error: 'poetry install' failed."
     exit 1
 fi
 #rm -rf /root/.cache/pypoetry/
+
 nohup /opt/monitor_marker.sh > /var/log/monitor_marker.log 2>&1 &
+echo "Starting the service..."
 exec python /opt/marker/marker_server.py --host 0.0.0.0 --port 80
 
